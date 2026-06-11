d28a7c04f02cfd1cca5139350c76db4cb8f671b1
Ravi Kumar Chaudhary <86255362+ravichaudhary111@users.noreply.github.com>
2026-06-11 12:25:41 +0530

    Time: 0 ms (100%), Space: 58.4 MB (28.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,14 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let i = j = 1;
+    let i = 1;
+    let j = 1;
     while (j < nums.length) {
-        if (nums[i - 1] != nums[j]) {
+        if (nums[i - 1] !== nums[j]) {
             nums[i] = nums[j];
             i++;
         }
         j++;
     }
     return i;
-
 };
