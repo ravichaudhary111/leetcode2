3fd8b2d28cfc7ea4edf4e3b9b02f5df79d4103cd
Ravi Kumar Chaudhary <86255362+ravichaudhary111@users.noreply.github.com>
2026-06-11 12:49:08 +0530

    Time: 1 ms (88.14%), Space: 57.3 MB (52.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    let val = nums[0];
-    let count = 0
-    for (let num of nums) {
-        count = val === num ? count + 1 : count - 1;
-        if (count === 0) {
-            val = num;
-            count++;
-        }
+  let val = nums[0];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    count = val === nums[i] ? ++count : --count;
+
+    if (count <= 0) {
+      val = nums[i];
+      count=1;
     }
-    return val;
+  }
+  return val;
 };
