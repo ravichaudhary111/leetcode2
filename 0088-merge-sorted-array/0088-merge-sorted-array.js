9f7764440af1556afd3e4a2adfffa3e4ba31687d
Ravi Kumar Chaudhary <86255362+ravichaudhary111@users.noreply.github.com>
2026-06-11 12:20:54 +0530

    Time: 0 ms (100%), Space: 54.2 MB (51.8%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    let i = m + n - 1;
+    let j = m - 1;
+    let k = n - 1;
+    while (k >= 0) {
+        if (nums1[j] > nums2[k]) {
+            nums1[i] = nums1[j];
+            j--;
+        } else {
+            nums1[i] = nums2[k];
+            k--;
+        }
+        i--;
+    }
+    return nums1;
+};
